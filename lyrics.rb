cfe76974f2d7717f02e2698bf8cadb3284034a77
Joe Wright <joe@joejag.com>
2013-01-29 13:06:08 +0000

    Adding a few comments

--- lyrics.rb
+++ lyrics.rb
@@ -9,6 +9,7 @@ song = clean(ENV['SONG'] ||= 'in bloom')
 
 song_link = nil
 begin
+    # Song titles on azlyrics can have numbers in them, search the artist listing for the song
     open("http://www.azlyrics.com/#{artist[0]}/#{artist}.html").each do |line|
         if line.include? song and line.include? '.html'
             song_link = line.split("\"")[1]
@@ -16,18 +17,19 @@ begin
         end
     end
 rescue OpenURI::HTTPError
-    puts 'No artist called that sorry'
-    exit
+    puts 'No artist called that sorry'; exit
 end
 
 if song_link.nil?
-    puts "No lyrics for that song sorry"
-    exit
+    puts "No lyrics for that song sorry"; exit
 end
 
 lyrics_started = false
+# azlyrics has html comments which makes it very easy to spot the start and end
 open("http://www.azlyrics.com/#{artist[0]}/#{song_link}").each do |line|
     break if line.include? '<!-- end of lyrics -->'
+    
+    # Remove any HTML gubbins from the lyrics
     puts line.gsub(%r{</?[^>]+?>}, '') if lyrics_started
     lyrics_started = true if line.include? '<!-- start of lyrics -->'
 end
