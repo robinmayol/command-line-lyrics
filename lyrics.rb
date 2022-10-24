b0ba8ba4dcc0a45035d3e0b2d97d54a55172c856
Robin M <robinm@posteo.net>
2022-10-24 20:35:39 +0100

    Updated in 2022 to search the website rather than try to build the address from the artist and song name as it often fails.

--- lyrics.rb
+++ lyrics.rb
@@ -4,11 +4,18 @@ def clean(string)
     string.downcase.gsub(' ', '')
 end
 
-def song_link_for(artist, song)
+searchartist = ENV['ARTIST']
+searchartist = searchartist.downcase.gsub(' ', '+')
+# print searchartist
+searchsong = ENV['SONG']
+searchsong = searchsong.downcase.gsub(' ', '+')
+# print searchsong
+def song_link_for(searchartist, searchsong)
     begin
+        # print "https://search.azlyrics.com/search.php?q=#{searchartist}+#{searchsong}&x=0f58cfda39487ca4460bd2df9b8b7c71769dd2df71e5724a1c8ffe1a20d47407"
         # Song titles on azlyrics can have numbers in them, search the artist listing for the song
-        open("http://www.azlyrics.com/#{artist[0]}/#{artist}.html").each do |line|
-            return line.split("\"")[1] if line.include? song and line.include? '.html'
+        URI.open("https://search.azlyrics.com/search.php?q=#{searchartist}+#{searchsong}&x=0f58cfda39487ca4460bd2df9b8b7c71769dd2df71e5724a1c8ffe1a20d47407",'User-Agent' => 'Mozilla').each do |line|
+            return line.split("\"")[1] if line.include? '/lyrics/' and line.include? '.html'
         end
         puts "No lyrics for that song sorry"; exit
     rescue OpenURI::HTTPError
@@ -16,17 +23,14 @@ def song_link_for(artist, song)
     end
 end
 
-artist = clean(ENV['ARTIST'] ||= 'nirvana')
-song = clean(ENV['SONG'] ||= 'in bloom')
-
-song_link = song_link_for(artist, song)
+song_link = song_link_for(searchartist, searchsong)
 
 lyrics_started = false
-open("http://www.azlyrics.com/#{artist[0]}/#{song_link}").each do |line|
+URI.open("#{song_link}",'User-Agent' => 'Mozilla').each do |line|
     # azlyrics has html comments which makes it very easy to spot the start and end
-    break if line.include? '<!-- end of lyrics -->'
+    break if line.include? '<!-- MxM banner -->'
 
     # Remove any HTML gubbins from the lyrics
     puts line.gsub(%r{</?[^>]+?>}, '') if lyrics_started
-    lyrics_started = true if line.include? '<!-- start of lyrics -->'
+    lyrics_started = true if line.include? '<!-- Usage of azlyrics.com '
 end
